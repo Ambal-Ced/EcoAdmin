205a29f34fcb3fe5302bfa5cf5990ad87f4c0509
Ambal_Ced <justineambal57@gmail.com>
2024-10-26 18:19:07 +0800

    Add project files.

---
+++ EcoAdmin/Areas/Identity/Data/ApplicationDbContext.cs
@@ -0,0 +1,35 @@
+﻿using EcoAdmin.Areas.Identity.Data;
+using EcoAdmin.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EcoAdmin.Areas.Identity.Data;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+    //Add Dbset
+    public DbSet<UserProfile> UserProfiles { get; set; }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+        builder.ApplyConfiguration(new ApplicationUserEntityConfiguration());
+    }
+}
+public class ApplicationUserEntityConfiguration : IEntityTypeConfiguration<ApplicationUser>
+{
+    public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+    {
+        builder.Property(x => x.FirstName);
+        builder.Property(x => x.LastName);
+        builder.Property(x => x.Type);
+    }
+}
---
+++ EcoAdmin/Models/UserProfile.cs
@@ -0,0 +1,17 @@
+﻿using EcoAdmin.Areas.Identity.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace EcoAdmin.Models
+{
+    public class UserProfile
+    {
+        [Key]
+        [Required]
+        public string UniqueUserName { get; set; }
+
+        // Foreign key reference to ApplicationUser
+        [Required]
+        public string ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+}
